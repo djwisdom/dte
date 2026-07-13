djwisdom/dte

commit 66c13d8b1dab6f74ab7725dd3d15aa39893967e5

Make use of `path_slice_basename()` in more places
See also: commit 8887d2763ae862a901bd6bb1fabd86b0bc00bba7

src/completion.c
@@ -150,10 +150,10 @@ static void collect_files(EditorState *e, CompletionState *cs, FileCollectionTyp
         fileprefix = strview(buf + dirprefix.length + 1);
         cs->tilde_expanded = true;
     } else {
-        const char *base = path_basename(cs->parsed);
-        bool has_slash = (base != cs->parsed);
+        size_t len = cs->head_len + cs->escaped.length + cs->tail.length;
+        fileprefix = path_slice_basename(string_view(cs->parsed, len));
+        bool has_slash = (fileprefix.data != cs->parsed);
         dirprefix = strview(has_slash ? dir : "");
-        fileprefix = strview(base);
     }
 
     do_collect_files(&cs->completions, dir, dirprefix, fileprefix, type);

src/filetype.c
@@ -357,7 +357,7 @@ bool is_valid_filetype_name_sv(StringView name)
 
 const char *filetype_str_from_extension(const char *path)
 {
-    StringView base = strview(path_basename(path));
+    StringView base = path_slice_basename(strview(path));
     StringView ext = get_filename_extension(base);
     FileTypeEnum ft = filetype_from_extension(ext);
     return (ft == NONE) ? NULL : builtin_filetype_names[ft];

src/load-save.c
@@ -381,7 +381,7 @@ static int tmp_file (
     }
 
     const StringView dir = path_slice_dirname(filename);
-    const StringView base = strview(path_basename(filename));
+    const StringView base = path_slice_basename(strview(filename));
     size_t required_buflen = dir.length + base.length + sizeof("/.tmp..XXXXXX");
 
     if (unlikely(buflen < required_buflen)) {

src/util/path.c
@@ -29,7 +29,6 @@ char *path_absolute(const char *path)
         return NULL;
     }
 
-    const char *base = path_basename(path);
     char *dir_relative = path_dirname(path);
     char *dir = realpath(dir_relative, NULL);
     free(dir_relative);
@@ -40,7 +39,7 @@ char *path_absolute(const char *path)
 
     // If the full path doesn't exist but the directory part does, return
     // the concatenation of the real directory and the non-existent filename
-    abs = path_join(dir, base);
+    abs = path_join(dir, path_basename(path));
     free(dir);
     return abs;
 }

commit 03dd22fb7dcc6f8fe3995995ee10972078469ed5

Fix regression in filename completion
Commit 66c13d8b1dab6f74ab mistakenly used an incorrect length for the
`StringView` passed to `path_slice_basename()` and consequently broke
completions for arguments like e.g. `open \G`. In the dte git repo,
for example, this should be completed as `open GNUmakefile` but this
bug was causing there to be no completion candidates at all.

src/completion.c
@@ -124,13 +124,13 @@ static bool do_collect_files (
 static void collect_files(EditorState *e, CompletionState *cs, FileCollectionType type)
 {
     char *dir = path_dirname(cs->parsed);
+    StringView parsed = strview(cs->parsed);
     StringView dirprefix;
     StringView fileprefix;
     char buf[8192];
 
     if (strview_has_prefix(cs->escaped, "~/")) {
         const StringView home = e->home_dir;
-        StringView parsed = strview(cs->parsed);
         BUG_ON(!strview_has_sv_prefix(parsed, home));
         strview_remove_prefix(&parsed, home.length + STRLEN("/"));
         bool sufficient_buf = parsed.length <= sizeof(buf) - sizeof("~/");
@@ -142,17 +142,16 @@ static void collect_files(EditorState *e, CompletionState *cs, FileCollectionTyp
             return;
         }
 
-        // Copy `cs->parsed` into `buf[]`, but with the $HOME/ prefix
-        // replaced with ~/
+        // Copy `parsed` into `buf[]`, but with the $HOME/ prefix replaced
+        // with ~/
         xmempcpy2(buf, STRN("~/"), parsed.data, parsed.length + 1);
 
         dirprefix = path_slice_dirname(buf);
         fileprefix = strview(buf + dirprefix.length + 1);
         cs->tilde_expanded = true;
     } else {
-        size_t len = cs->head_len + cs->escaped.length + cs->tail.length;
-        fileprefix = path_slice_basename(string_view(cs->parsed, len));
-        bool has_slash = (fileprefix.data != cs->parsed);
+        fileprefix = path_slice_basename(parsed);
+        bool has_slash = (fileprefix.data != parsed.data);
         dirprefix = strview(has_slash ? dir : "");
     }
 