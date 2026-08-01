e196cbe1cb6371343cc207e1c1fca1ac97869d52
Craig Barnes <craigbarnes@protonmail.com>
2026-08-01 20:32:25 +0100

    Clean up and improve changes to `showkey_loop()` done in previous commit
    See commit: 53343ece52bec6c2868d6cb86dbe4380d0f15924

--- src/showkey.c
+++ src/showkey.c
@@ -19,12 +19,6 @@ static bool is_ignored_key(KeyCode key, Terminal *term)
         return true;
     }
 
-    if (key == KEYCODE_REDRAW) {
-        // Move cursor to column 1 (CR) and erase current line (EL)
-        term_put_literal(&term->obuf, "\r\033[K");
-        return true;
-    }
-
     return key == KEY_NONE;
 }
 
@@ -45,19 +39,33 @@ ExitCode showkey_loop(unsigned int terminal_query_level)
     term_put_literal(obuf, "\rPress any key combination, or use Ctrl+D to exit\r\n");
     term_output_flush(obuf);
 
-    char buf[KEYCODE_STR_BUFSIZE + STRLEN("  \r\n")];
-    size_t prefix_len = copyliteral(buf, "  ");
-
+    char line[KEYCODE_STR_BUFSIZE + 8];
     for (KeyCode key = KEY_NONE; key != (MOD_CTRL | 'd'); ) {
         key = term_read_input(&term, 100);
         if (is_ignored_key(key, &term)) {
             continue;
         }
 
-        size_t n = prefix_len;
-        n += keycode_to_str(key, buf + n);
-        n += copyliteral(buf + n, "\r\n");
-        (void)!xwrite_all(STDOUT_FILENO, buf, n);
+        size_t n = 0;
+        if (key == KEYCODE_REDRAW) {
+            // Move cursor to column 1 (CR) and erase current line (EL),
+            // in case the call chain:
+            //
+            // • term_read_input() →
+            // • term_read_input_modern() →
+            // • term_handle_query_reply() →
+            // • term_put_level_2_queries()
+            //
+            // … emitted something that the terminal decided to print,
+            // instead of following the ECMA-48 parsing rules.
+            n += copyliteral(line + n, "\r\033[K");
+        } else {
+            n += copyliteral(line + n, "  ");
+            n += keycode_to_str(key, line + n);
+            n += copyliteral(line + n, "\r\n");
+        }
+
+        (void)!xwrite_all(STDOUT_FILENO, line, n);
     }
 
     term_restore_private_modes(&term);
